7f26a9f9714aad05c1ac626c345eb480a3208120
Alex Summer <alex.summer48@yahoo.com>
2020-01-21 22:47:53 -0500

    New features
    Got the etching and sketching working along with the resize button. Clear button has some performance issues.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,34 @@
-const mainGrid = document.getElementById
+const mainGrid = document.getElementById('gridContainer');
+const resizeButton = document.getElementById('resizeButton')
+const clearButton = document.getElementById('clearButton')
 
-function main(){
 
-
-
-}
-
-function createGrid(gridAmount){
+function createGrid(gridAmount,){
+  mainGrid.innerHTML = ''
   for(var i = 0; i < (gridAmount ** 2); i++){
-
+    mainGrid.setAttribute('style', `grid-template-columns: repeat(${gridAmount}, 1fr); grid-template-rows: repeat(${gridAmount}, 1fr);`);
+    const squareDiv = document.createElement('div');
+    mainGrid.appendChild(squareDiv);
   }
 }
+
+createGrid(36);
+mainGrid.addEventListener('mouseover', (e) =>{
+  if (e.target.parentElement.id === 'gridContainer'){
+    e.target.style.backgroundColor = 'black';
+  }
+})
+resizeButton.addEventListener('click', () => {
+  var gridSize = prompt('What size would you like the grid to be?');
+  if (gridSize == ''){
+    alert('Please enter in a number')
+  }
+  else{
+    createGrid(gridSize);
+  }
+})
+clearButton.addEventListener('click', () =>{
+  var gridCount = mainGrid.childNodes.length;
+  createGrid(gridCount);
+  //odd performance issues. Figure it out tomorrow
+})
